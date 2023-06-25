c0cd047d9c0f27cf3d6335e59541fc110c6b7fab
Rafael Ernesto <rflernesto@gmail.com>
2023-06-25 16:53:16 -0400

    feat: add singly linked list

---
+++ src/main/kotlin/SinglyLinkedList.kt
@@ -0,0 +1,71 @@
+sealed class List<out A> {
+    companion object {
+        fun <A> of(vararg a: A): List<A> {
+            val tail = a.sliceArray(1 until a.size)
+            return if (a.isEmpty()) Nil else Cons(a[0], of(*tail))
+        }
+
+        fun <A> tail(list: List<A>): List<A> {
+            return when(list) {
+                is Nil -> Nil
+                is Cons -> list.tail
+            }
+        }
+
+        fun <A> setHead(newHead: A, list: List<A>): List<A> {
+            return Cons(newHead, list)
+        }
+
+        fun <A> getHead(list: List<A>): A? {
+            return when(list) {
+                is Nil -> null
+                is Cons -> list.head
+            }
+        }
+
+        fun <A> drop(n: Int, list: List<A>): List<A> {
+            if(n <= 0) return list
+           return  when(list) {
+                is Nil -> Nil
+                is Cons -> drop(n - 1, list.tail)
+            }
+        }
+
+        fun <A> dropWhile(fn: (A) -> Boolean, list: List<A>): List<A> {
+            val consList = list as Cons
+            consList.head
+            if (!fn(list.head)) return list
+            return when(list) {
+                is Nil -> Nil
+                is Cons -> dropWhile(fn, list.tail)
+            }
+        }
+
+        fun <A> init(list: List<A>): List<A> {
+            val consList = list as Cons
+            if(consList.tail == Nil) {
+                return Nil
+            }
+            val returnedList = init(consList.tail)
+            return Cons(list.head, returnedList)
+        }
+
+        fun <A, B> foldRight(list: List<A>, z: B, fn: (A,B) -> B): B {
+            return when(list) {
+                is Nil -> z
+                is Cons -> fn(list.head, foldRight(list.tail, z, fn))
+            }
+        }
+
+        fun sum(list: List<Int>): Int =
+            foldRight(list, 0) { a, b -> a + b }
+
+        fun product(list: List<Int>): Double =
+            foldRight(list, 1.0) { a, b -> a * b }
+    }
+}
+
+object Nil: List<Nothing>()
+
+data class Cons<out A>(val head: A, val tail: List<A>): List<A>()
+
