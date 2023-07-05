b49235b5fd0b28bb5d1cbdf25a5c182f83815160
Rafael Ernesto <rflernesto@gmail.com>
2023-07-05 19:33:59 -0400

    chore: add map

--- src/main/kotlin/Either.kt
+++ src/main/kotlin/Either.kt
@@ -1,4 +1,13 @@
+import java.lang.Exception
+
 sealed class Either<out E, out A>
 
 data class Left<out E>(val value: E) : Either<E, Nothing>()
 data class Right<out A>(val value: A) : Either<Nothing, A>()
+
+fun <A, B, E> Either<E, A>.map(fn: (A) -> B): Either<E, B> {
+   return when(this) {
+       is Left -> this
+       is Right -> Right(fn(this.value))
+   }
+}
